d332e1c1f4548fe6ff398af9da771528fc012ad3
moatorres <44585769+moatorres@users.noreply.github.com>
2023-07-27 20:03:07 -0300

    fix(app): `useHTTPS` now conditional to `.env`

--- apps/gzipr/src/app/server.ts
+++ apps/gzipr/src/app/server.ts
@@ -1,13 +1,12 @@
-import { ConsoleLogger } from '@gzipr/core'
 import { config } from '../config'
-import { ExpressServer, RoutesMiddleware } from '@gzipr/core'
+import { ConsoleLogger, ExpressServer, RoutesMiddleware } from '@gzipr/core'
 
 export const logger = new ConsoleLogger()
 
 export const server = ExpressServer.create({
   config: config,
   logger: logger,
-  useHTTPS: false,
+  useHTTPS: Boolean(config.get('USE_HTTPS')),
 })
 
 export const notFound = new RoutesMiddleware({
