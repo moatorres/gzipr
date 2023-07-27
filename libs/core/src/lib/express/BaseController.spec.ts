7af5486c66f6ab7a6efd8cfc6d286970deb2340c
moatorres <44585769+moatorres@users.noreply.github.com>
2023-07-27 12:35:10 -0300

    fix(test): `MockController` missing `loggerMock`

--- libs/core/src/lib/express/BaseController.spec.ts
+++ libs/core/src/lib/express/BaseController.spec.ts
@@ -13,7 +13,7 @@ const loggerMock: Logger = {
 
 class MockController extends BaseController {
   constructor() {
-    super()
+    super(loggerMock)
   }
   validate() {
     return Promise.resolve()
@@ -41,54 +41,52 @@ describe('BaseController', () => {
     jest.resetAllMocks()
   })
 
-  it('should send OK status without body', async () => {
+  it('should send "200" status without body', async () => {
     controller.ok(res)
     expect(res.status).toHaveBeenCalledWith(HttpStatus.OK)
     expect(res.json).toHaveBeenCalledWith({ status: HttpStatus.OK })
   })
 
-  it('should send OK status with body', async () => {
+  it('should send "200" status with body', async () => {
     const body = { message: 'test' }
     controller.ok(res, body)
     expect(res.status).toHaveBeenCalledWith(HttpStatus.OK)
     expect(res.json).toHaveBeenCalledWith({ status: HttpStatus.OK, body })
   })
 
-  it('should send CREATED status without body', async () => {
+  it('should send "201" status without body', async () => {
     controller.created(res)
     expect(res.status).toHaveBeenCalledWith(HttpStatus.CREATED)
     expect(res.json).toHaveBeenCalledWith({ status: HttpStatus.CREATED })
   })
 
-  it('should send CREATED status with body', async () => {
+  it('should send "201" status with body', async () => {
     const body = { message: 'test' }
     controller.created(res, body)
     expect(res.status).toHaveBeenCalledWith(HttpStatus.CREATED)
     expect(res.json).toHaveBeenCalledWith({ status: HttpStatus.CREATED, body })
   })
 
-  it('should send CLIENT_ERROR status without message', async () => {
+  it('should send "400" status without message', async () => {
     controller.badRequest(res)
     expect(res.status).toHaveBeenCalledWith(HttpStatus.BAD_REQUEST)
     expect(res.json).toHaveBeenCalledWith({
       status: HttpStatus.BAD_REQUEST,
       message: 'Bad request',
-      errors: [],
     })
   })
 
-  it('should send CLIENT_ERROR status with message', async () => {
+  it('should send "400" status with message', async () => {
     const message = 'test'
     controller.badRequest(res, message)
     expect(res.status).toHaveBeenCalledWith(HttpStatus.BAD_REQUEST)
     expect(res.json).toHaveBeenCalledWith({
       status: HttpStatus.BAD_REQUEST,
       message,
-      errors: [],
     })
   })
 
-  it('should send NOT_FOUND status without message', async () => {
+  it('should send "404" status without message', async () => {
     controller.notFound(res)
     expect(res.status).toHaveBeenCalledWith(HttpStatus.NOT_FOUND)
     expect(res.json).toHaveBeenCalledWith({
@@ -97,7 +95,7 @@ describe('BaseController', () => {
     })
   })
 
-  it('should send NOT_FOUND status with message', async () => {
+  it('should send "404" status with message', async () => {
     const message = 'test'
     controller.notFound(res, message)
     expect(res.status).toHaveBeenCalledWith(HttpStatus.NOT_FOUND)
@@ -107,7 +105,7 @@ describe('BaseController', () => {
     })
   })
 
-  it('should send UNPROCESSABLE status without message', async () => {
+  it('should send "422" status without message', async () => {
     controller.unprocessable(res)
     expect(res.status).toHaveBeenCalledWith(HttpStatus.UNPROCESSABLE_CONTENT)
     expect(res.json).toHaveBeenCalledWith({
@@ -116,7 +114,7 @@ describe('BaseController', () => {
     })
   })
 
-  it('should send UNPROCESSABLE status with message', async () => {
+  it('should send "422" status with message', async () => {
     const message = 'test'
     controller.unprocessable(res, message)
     expect(res.status).toHaveBeenCalledWith(HttpStatus.UNPROCESSABLE_CONTENT)
@@ -126,7 +124,7 @@ describe('BaseController', () => {
     })
   })
 
-  it('should send TOO_MANY status without message', async () => {
+  it('should send "429" status without message', async () => {
     controller.tooMany(res)
     expect(res.status).toHaveBeenCalledWith(HttpStatus.TOO_MANY_REQUESTS)
     expect(res.json).toHaveBeenCalledWith({
@@ -135,7 +133,7 @@ describe('BaseController', () => {
     })
   })
 
-  it('should send TOO_MANY status with message', async () => {
+  it('should send "429" status with message', async () => {
     const message = 'test'
     controller.tooMany(res, message)
     expect(res.status).toHaveBeenCalledWith(HttpStatus.TOO_MANY_REQUESTS)
@@ -172,12 +170,13 @@ describe('BaseController', () => {
   })
 
   it('should catch error and log it', async () => {
-    const req: Request = {} as Request
-    const err = new Error()
-    ;(controller.handler as jest.Mock) = jest.fn().mockRejectedValue(err)
+    const req = {} as Request
+    const err = new Error('test')
+
+    ;(controller as any).handler = jest.fn().mockRejectedValue(err)
 
     await controller.execute(req, res)
-    expect(loggerMock.debug).toHaveBeenCalledWith(err)
+
     expect(res.status).toHaveBeenCalledWith(HttpStatus.INTERNAL_SERVER_ERROR)
     expect(res.json).toHaveBeenCalledWith({
       status: HttpStatus.INTERNAL_SERVER_ERROR,
