b0054681ff09d5e06816dcedba7714fd3a193ed6
moatorres <44585769+moatorres@users.noreply.github.com>
2023-07-28 11:06:55 -0300

    fix(app): use `number` instead of `boolean`

--- apps/gzipr/src/app/server.ts
+++ apps/gzipr/src/app/server.ts
@@ -6,7 +6,7 @@ export const logger = new ConsoleLogger()
 export const server = ExpressServer.create({
   config: config,
   logger: logger,
-  useHTTPS: Boolean(config.get('USE_HTTPS')),
+  useHTTPS: Number(config.get('USE_HTTPS')) === 1,
 })
 
 export const notFound = new RoutesMiddleware({
